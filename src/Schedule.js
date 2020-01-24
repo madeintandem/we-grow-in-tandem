fd38faa6025f4b840dbcb9e5648bc2cbe335a3b1
Pooja Saxena <pooja335@gmail.com>
2020-01-24 15:36:45 -0600

    remove lots of extra unused code, import calendar, start logic for parsing plants

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,24 +1,47 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import * as plants from './default_input_data.json';
 
-export default function Schedule() {
+window.moment = moment
+const getWateringEvents = () => {
+  let allEvents = []
+  debugger
+  const firstDay = moment().day(8)
+  // const lastDay = moment().day
+  JSON.parse(plants).forEach(plant => {
+
+  })
+}
+
+
+
+export function Schedule() {
+  const localizer = momentLocalizer(moment)
+
+  console.log(moment().day(8))
+  getWateringEvents()
+
+
+  // Event {
+  //   title: string,
+  //   start: Date,
+  //   end: Date,
+  //   allDay?: boolean
+  //   resource?: any,
+  // }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
